07e393f4e30886c0509d223d85da92e1c38f401c
ADMIN <tooalbert79@gmail.com>
2024-09-22 00:11:47 +0300

    Customer service implemented

---
+++ Services/Customer/src/main/java/com/example/ecommerce/Customer/Address.java
@@ -0,0 +1,16 @@
+package com.example.ecommerce.Customer;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.validation.annotation.Validated;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Validated
+public class Address {
+    public String zipcode;
+}
