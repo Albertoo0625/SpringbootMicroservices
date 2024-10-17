e16cab3b22aefb003fbcee854f955b64c1dc6636
ADMIN <tooalbert79@gmail.com>
2024-10-17 11:32:15 +0300

    product service configured

---
+++ Services/Product/src/main/java/com/example/ecommerce/product/Product.java
@@ -0,0 +1,27 @@
+package com.example.ecommerce.product;
+
+import com.example.ecommerce.category.Category;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "product_seq")
+    @SequenceGenerator(name = "product_seq", sequenceName = "product_seq", allocationSize = 1)
+    private Integer id;
+    private String name;
+    private String description;
+    private Double availableQuantity;
+    private BigDecimal price;
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
